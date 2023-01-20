fc95265996129535e645c26fdd976f7b5317f6c2
Irina Gorynina <iren.gorynina@gmail.com>
2023-01-21 00:24:08 +0300

    Решение 38 задачи

---
+++ Exercise38/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива
+Console.WriteLine("Введите размер массива:  ");
+int size = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите минимальное число массива: ");
+double A = Math.Round(new Random().NextDouble() + new Random().Next(0,9),2);
+Console.WriteLine("Введите максимальное число массива: ");
+double B = Math.Round(new Random().NextDouble() + new Random().Next(0,9),2);
+int [] numbers = new int[size];
+
+FillArrayRandomNumbers(numbers);
+PrintArray(numbers);
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    if (numbers[i] > B)
+    {
+        double max = numbers[i];
+    }
+    if (numbers[i] < A)
+    {
+        double min = numbers[i];
+    }
+}
+Console.WriteLine($"Разница между максимальным и минимальным числом = {B - A}");
+
+void FillArrayRandomNumbers(int [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(min, max);
+    }
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
